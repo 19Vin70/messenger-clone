a02f4a894b4f52f31ce2ce1f685103e9744eaf47
Marvin Quillo Saik <113106676+19Vin70@users.noreply.github.com>
2023-08-28 11:58:43 +0800

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("open").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.add("active");
+});
+
+document.getElementById("close").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.remove("active");
+});
+
+
