750d11814eb3a4ddb89e6aef838167bc476ff3fd
Marvin Quillo Saik <113106676+19Vin70@users.noreply.github.com>
2023-08-29 09:38:26 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,48 @@ document.getElementById("close").addEventListener("click", function() {
 });
 
 
+const navbarLinks = document.querySelectorAll('.navbar a');
+
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            navbarLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            this.classList.add('active');
+
+            const href = this.getAttribute('href');
+
+            window.location.href = href;
+        });
+    });
+
+
+
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const seeAllButton = document.getElementById("seeAllButton");
+        const backButton = document.getElementById("backButton");
+        const profiles = document.querySelectorAll(".peoples-profile");
+        const hiddenProfiles = Array.from(profiles).slice(3);
+
+        seeAllButton.addEventListener("click", function () {
+            hiddenProfiles.forEach(profile => {
+                profile.style.display = "flex";
+            });
+            seeAllButton.style.display = "none";
+            backButton.style.display = "flex";
+        });
+
+        backButton.addEventListener("click", function () {
+            hiddenProfiles.forEach(profile => {
+                profile.style.display = "none";
+            });
+            seeAllButton.style.display = "flex";
+            backButton.style.display = "none";
+        });
+    });
+    
